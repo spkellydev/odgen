09dd0118249cac5467664eb45fef38d947b58087
Sean Kelly <spkellydev@gmail.com>
2018-09-06 21:21:58 -0400

    loading css

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
 <?php
+if (PHP_SAPI == 'cli-server') {
+    $_SERVER['SCRIPT_NAME'] = basename(__FILE__);
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
 
 require __DIR__ . '/../vendor/autoload.php';
 use App\Bootstrap as App;
